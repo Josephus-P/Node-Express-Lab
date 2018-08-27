9c83e8e1675cc682494787b918354b554131ff5d
Josue Peralta <jperal77@gmail.com>
2018-08-27 17:13:01 -0500

    added get request for all posts

--- server.js
+++ server.js
@@ -3,3 +3,22 @@
 const db = require('./data/db.js');
 
 // add your server code starting here
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Hello CS12');
+});
+
+server.get('/api/posts', (req, res) => {
+    db.find().then(posts => {
+        res.status(200).json(posts);
+    }).catch(err => {
+        console.error('Error', err);
+        res.status(500).json({message: "Error"});
+    });
+});
+server.listen(9000, () => console.log('\n== API on port 9k ==\n'));
